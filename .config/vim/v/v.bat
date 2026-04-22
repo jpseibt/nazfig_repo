1542abbe8cb03acccabc8f860c1b33e2daa5cdf4
J. Paulo Seibt <jpseibt@gmail.com>
2026-04-22 16:57:39 -0300

    Fix `--vim` flag behavior of v batch script, and fix netrw tab renaming

--- .config/vim/v/v.bat
+++ .config/vim/v/v.bat
@@ -1,26 +1,36 @@
 @echo off
 
-:: v [--vim] <-e, -t, -d> [--edit] [Vim/Nvim arguments]
-:: Possible commands ...
-:: Neovide/NeoVim:
-::   > v (`nvim --server $NVIM --remote .`)
-::   > v -e <NVIM ARGS>   (`nvim --server $NVIM --remote <NVIM ARGS>`)
-::   > v -t <NVIM ARGS>   (`nvim --server $NVIM --remote-tab <NVIM ARGS>`)
-::   > v -d <file> <file> (`nvim --server $NVIM --remote-tab <NVIM ARGS>`)
-:: Vim:
-::   > v --vim (`vim --server VIM --remote .`)
-::   > v --vim -e <VIM ARGS> (`vim --server VIM --remote <VIM ARGS>`)
-::   > v --vim -t <VIM ARGS> (`vim --server VIM --remote-tab <VIM ARGS>`)
-::   > v --vim -d {file_a} {file_b} (
-::     `vim --server VIM --remote-tab {file_a}`
-::     `vim --server VIM --remote-send ":vert split | edit {file_b} | windo diffthis`
-::   )
+:: Usage:
+::   v [--vim] [--edit | --term] [-e | -t | -d] [files...]
+::
+:: Description:
+::   Generate terminal-to-host / remote Vim or Nvim command calls
+::
+:: Options:
+::   --vim     Use Vim instead of Neovim (must be the first argument)
+::   -e        Edit file in the current window (default)
+::   -t        Edit file in a new tab
+::   -d        Open two files in a new tab and `windo diffthis`
+::   --edit    Open the `v` script for editing (at %userprofile%\vimfiles\v\v.bat)
+::   --term    Open current directory (netrw) and launch germinal on a new tab
+::
+:: Examples:
+::   v .                   Open current directory     ($ nvim --server %NVIM% --remote .)
+::   v --vim -t main.c     Open `main.c` in a new tab ($ vim --servername SERVER --remote-tab main.c)
+::   v -d file_a file_b    Diff `file_a` (lhs) and `file_b` (rhs)
+::     ($ nvim --server %NVIM% --remote-tab file_a
+::      $ nvim --server %NVIM% --remote-send "<C-\><C-n> :execute 'vert split ' . fnameescape('file_b') | windo diffthis<CR>")
+::
+::  Note: if there's no Vim or Neovim server, launch new Vim/Neovide instance
 
-set "V_PRG="
-set "V_SERVER="
+
+set "V_PRG=nvim"
+set "V_SERVER_OP=--server"
 set "V_REMOTE_OP=--remote"
+set "V_SERVER=%NVIM%"
 set "V_ARGS="
 set "V_DIFF=0"
+set "V_TERM=0"
 set "V_FILE_A="
 set "V_FILE_B="
 
@@ -32,11 +42,15 @@ set "V_FILE_B="
 :: Check for the `--vim` (only on %1) flag to set V_PRG to Vim or NeoVim
 if "%~1" == "--vim" (
   set "V_PRG=vim"
-  set "V_SERVER=VIM"
+  set "V_SERVER_OP=--servername"
+  set "V_SERVER="
+
+  for /f "tokens=*" %%i in ('vim --serverlist') do (
+    set "V_SERVER=%%i"
+    goto for_out
+  )
+  :for_out
   shift
-) else (
-  set "V_PRG=nvim"
-  set "V_SERVER=%NVIM%"
 )
 
 :: Set var based on the flag `-e = --remote`, `-t` = remote-tab)
@@ -48,20 +62,26 @@ if "%~1" == "-e" (
 ) else if "%~1" == "-d" (
   set "V_DIFF=1"
   set "V_FILE_A="%~f2""
-  set "V_FILE_B="%~f3""
+  set "V_FILE_B=%~f3"
   goto end
 )
+
+if %V_DIFF% == 0 (
+  if "%~1" == "" (
+    set "V_ARGS=."
+    goto end
+  ) else if "%~1" == "--edit" (
+    set "V_ARGS="%userprofile%\vimfiles\v\v.bat""
+    goto end
+  ) else if "%~1" == "--term" (
+    set "V_ARGS=."
+    set "V_TERM=1"
+    goto end
+  )
+)
+
 ::echo [DEBUG] V_FILE_A: %V_FILE_A%
 ::echo [DEBUG] V_FILE_B: %V_FILE_B%
-
-:: If there are no arguments left, default to current directory (`--edit` = edit v.bat)
-if "%~1" == "" (
-  set "V_ARGS=."
-  goto end
-) else if "%~1" == "--edit" (
-  set "V_ARGS="%userprofile%\vimfiles\v\v.bat""
-  goto end
-)
 
 :: Build variable with every arg from the new %1 onwards
 :loop
@@ -98,19 +118,26 @@ goto loop
 
 :: If V_PRG is nvim and there's no Neovide instance, define a server pipe name
 :: and launch Neovide, passing --listen to nvim (sleep to wait for Neovide to open)
-if "%NVIM%" == "" (
+if "%V_SERVER%" == "" (
   if "%V_PRG%" == "nvim" (
     set "NVIM=\\.\pipe\neovide_v_server"
     set "V_SERVER=\\.\pipe\neovide_v_server"
     start neovide -- --listen \\.\pipe\neovide_v_server
-    timeout /t 1 /nobreak >nul
+  ) else (
+    set "V_SERVER=VIM_v_server"
+    start vim --servername VIM_v_server
   )
+  timeout /t 1 /nobreak >nul
 )
 
 :: Launch the V_PRG with server (NeoVim server path is stored in %NVIM% or custom pipe)
 if "%V_DIFF%" == "1" (
-  %V_PRG% --server %V_SERVER% --remote-tab %V_FILE_A%
-  %V_PRG% --server %V_SERVER% --remote-send "<C-\><C-n>:execute 'vert split ' . fnameescape('%V_FILE_B%') | windo diffthis<CR>"
+  %V_PRG% %V_SERVER_OP% %V_SERVER% --remote-tab %V_FILE_A%
+  %V_PRG% %V_SERVER_OP% %V_SERVER% --remote-send "<C-\><C-n> :execute 'vert split ' . fnameescape('%V_FILE_B%') | windo diffthis<CR>"
 ) else (
-  %V_PRG% --server %V_SERVER% %V_REMOTE_OP% %V_ARGS%
+  %V_PRG% %V_SERVER_OP% %V_SERVER% %V_REMOTE_OP% %V_ARGS%
+  if %V_TERM% == 1 (
+    timeout /t 1 /nobreak >nul
+    %V_PRG% %V_SERVER_OP% %V_SERVER% --remote-send "<C-\><C-n> :Termhere<CR>"
+  )
 )
